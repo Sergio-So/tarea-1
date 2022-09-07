98c89c82ba7ce59ed6c85988741cac9b8c91e303
Sergio-So <111761716+Sergio-So@users.noreply.github.com>
2022-09-07 13:24:53 -0500

    Update number.cpp

--- number.cpp
+++ number.cpp
@@ -10,33 +10,33 @@ char number_t::get_type(){
 }
 integer_t::integer_t(int n):number_t(n){
   type='i';
-  int b=rand()%2;
-  if (b==1){
-    res=n*-1;
-  }
-  else {
-    res=n;
-  }
-  numero=res;
+//   int b=rand()%2;
+//   if (b==1){
+//     res=n*-1;
+//   }
+//   else {
+//     res=n;
+//   }
+//   numero=res;
 }
 real_t::real_t(double n):number_t(n){
   type='r';
-  int b=rand()%2;
-  int c=rand()%2;
-  if (b==0){
-    if(c==1){
-      res=-1*n;
-    }
-    else{res=n;}
-  }
-  if(b==1){
-    if(c==1){
-      res=-1*(n*(rand()%100))/100;
-    }
-    else{
-    res=(n*(rand()%100))/100;}
-  }
-  numero=res;
+//   int b=rand()%2;
+//   int c=rand()%2;
+//   if (b==0){
+//     if(c==1){
+//       res=-1*n;
+//     }
+//     else{res=n;}
+//   }
+//   if(b==1){
+//     if(c==1){
+//       res=-1*(n*(rand()%100))/100;
+//     }
+//     else{
+//     res=(n*(rand()%100))/100;}
+//   }
+//   numero=res;
 }
 double number_t::num(){
     return numero;
