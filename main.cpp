606c517150796c23af7924ae5e042f888ee0fef0
Sergio-So <111761716+Sergio-So@users.noreply.github.com>
2022-09-07 12:59:55 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 int main() {
   srand(time(NULL));
-  utec::matrix_t m1(4, 5);
+  utec::matrix m1(4, 5);
     for (int i = 0; i < m1.row_size(); ++i) {
         for (int j = 0; j < m1.col_size(); ++j) {
              m1(i, j) = new integer_t(rand() % 100);
         }
     }
     std::cout <<"Matrix 1:"<<endl<<m1;
-    utec::matrix_t m2 = m1;
+    utec::matrix m2 = m1;
     
     for (int i = 0; i < m2.row_size(); ++i) {
         for (int j = 0; j < m2.col_size(); ++j) {
