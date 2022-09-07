53f7f994ddffb96d549c0df19f3df270f8d498ef
Sergio-So <111761716+Sergio-So@users.noreply.github.com>
2022-09-07 12:56:28 -0500

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,21 +1,21 @@
 #include "matrix.h"
-#include <time.h>
-using namespace std;
-int main() {
-  srand(time(NULL));
-  utec::matrix m1(4, 5);
-    for (int i = 0; i < m1.row_size(); ++i) {
-        for (int j = 0; j < m1.col_size(); ++j) {
-             m1(i, j) = new integer_t(rand() % 100);
+using namespace utec;
+matrix::matrix(int fil, int col):fila(fil),columna(col){
+  matrix_1.resize(fila);
+  for(int i=0;i<fila;i++){
+        matrix_1[i].resize(columna);
+
+        for(int j=0;j<columna;j++){
+            matrix_1[i][j]=nullptr;
         }
     }
-    std::cout <<"Matrix 1:"<<endl<<m1;
-    utec::matrix m2 = m1;
-    
-    for (int i = 0; i < m2.row_size(); ++i) {
-        for (int j = 0; j < m2.col_size(); ++j) {
-            m2(i, j) = new real_t(rand() % 100);
-        }
-    }
-    std::cout <<"Matrix 2:"<<endl<<m2;
+}
+int matrix::row_size(){
+  return fila;
+}
+int matrix::col_size(){
+  return columna;
+}
+number_t* &matrix::operator() (int i, int j) {
+    return matrix_1[i][j];
 }
