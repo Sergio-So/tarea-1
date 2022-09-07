04cb235749af9fb1329a9d98948bf14e6f3ca8c1
Sergio-So <111761716+Sergio-So@users.noreply.github.com>
2022-09-07 13:17:34 -0500

    Update matrix.h

--- matrix.h
+++ matrix.h
@@ -2,17 +2,17 @@
 #include "number.h"
 using namespace std;
 namespace utec{
-  class matrix{
+  class matrix_t{
     private:
     vector<vector<number_t*>>matrix_1;
     int fila,columna;
     public:
-    matrix(int fil, int col);
+    matrix_t(int fil, int col);
     int row_size();
     int col_size();
     number_t* &operator()(int i, int j);
-    matrix& operator=(matrix& m);
-    friend ostream& operator<<(ostream& salida,matrix& m){
+    matrix_t& operator=(matrix& m);
+    friend ostream& operator<<(ostream& salida,matrix_t& m){
       for(int i=0;i< m.row_size();i++){
         for(int j=0;j<m.col_size();j++){
         if(m(i,j)->get_type()=='i')
